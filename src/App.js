2d471179cb89bc811831c4ad780c6a47ed6d09d8
abhakuma <abhakuma@c02f5501md6m.ent.gartner.com>
2021-04-05 17:21:31 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react";
+import { shadowizard } from "@abhaykumar01234/shadowizard";
 
 function App() {
+  useEffect(() => {
+    shadowizard({
+      shadow_type: "soft",
+      padding: true
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" />
+        <div className="img-bg">
+          <img src="https://via.placeholder.com/250x200.png?text=image1" alt="image1" className="shadowizard" />
+          <img src="https://via.placeholder.com/250x200.png?text=image2" alt="image2" className="shadowizard" />
+          <img src="https://via.placeholder.com/250x200.png?text=image3" alt="image3" className="shadowizard" />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
